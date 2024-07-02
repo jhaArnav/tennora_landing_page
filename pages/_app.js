210ec60db86ba25ef325588b3253c5ef4d720233
Arnav Jha <arnavjha@Arnavs-MacBook-Pro-4.local>
2024-07-02 00:15:27 -0400

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 // pages/_app.js
 import '../app/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Tennora - Streamline Your Real Estate Business</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
