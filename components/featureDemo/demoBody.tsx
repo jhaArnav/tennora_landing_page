20b63a00241d7bf38b262ac540f947251567616f
Gryphon-Patlin <gryphonpatlin@gmail.com>
2024-07-01 22:42:28 -0600

    Added renamed files

---
+++ components/featureDemo/demoBody.tsx
@@ -0,0 +1,44 @@
+import NavBar from './navBar/navBar';
+import LandingNavBar from '../navBar';
+
+import ListingsPage from './listingsPage/body';
+import LeadsPage from './leadsPage/body';
+import DocumentPage from './documentsPage/body';
+import SchedulingPage from './schedulingPage/body';
+import MarketingPage from './marketingPage/body';
+import { useState } from 'react';
+
+export default function DemoBody() {
+    const [page, setPage] = useState<any | null>("leads");
+    return (
+        <div>
+            <LandingNavBar/>
+            <div className="p-10 pt-24 h-[60vw] bg-gray-100">
+                <div className="rounded-2xl border-black border-solid w-full h-full overflow-hidden shadow-xl">
+                    <NavBar router={setPage}/>
+                    <div className="w-full h-full">
+                        {renderSwitch(page)}
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    );
+}
+
+function renderSwitch(param: string) {
+    switch (param) {
+        case 'listings':
+            return <ListingsPage/>
+        case 'leads':
+            return <LeadsPage/>
+        case 'scheduling':
+            return <SchedulingPage/>
+        case 'documents':
+            return <DocumentPage/>
+        case 'marketing':
+            return <MarketingPage/>
+        default:
+            return '404 Not Found'
+    }
+}
---
+++ pages/demo.tsx
@@ -0,0 +1,8 @@
+import react from 'react';
+import DemoBody from '../components/Demo/demoBody';
+
+export default function Demo() {
+    return (
+        <DemoBody/>
+    );
+};
