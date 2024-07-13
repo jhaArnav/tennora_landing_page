eb45730d0948f0159479e00aed7a21773a616f9c
Arnav Jha <arnavjha@Arnavs-MacBook-Pro-4.local>
2024-07-13 02:08:08 -0400

    pricing page fixes

--- components/pricingPage/index.tsx
+++ components/pricingPage/index.tsx
@@ -79,6 +79,12 @@ const PricingPage = () => {
                         <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414L8 15.414 4.293 11.707a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                         </svg>
+                        Optional Phone Call transcription
+                    </li>
+                    <li className="flex items-center text-sm text-gray-700">
+                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414L8 15.414 4.293 11.707a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                        </svg>
                         24/7 Hotline Support
                     </li>
                     <li className="flex items-center text-sm text-gray-700">
