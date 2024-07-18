8070578e2f222eb543ab553f53d61c073c768168
Arnav Jha <arnavjha@Arnavs-MacBook-Pro-4.local>
2024-07-18 07:25:42 -0400

    resolve demo page errors

--- pages/demo.tsx
+++
@@ -1,8 +0,0 @@
-// import react from 'react';
-// import DemoBody from '../components/demo/demoBody';
-
-// export default function Demo() {
-//     return (
-//         <DemoBody/>
-//     );
-// };
