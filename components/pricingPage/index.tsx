18031819f2c0df8ea11bc6d95b3b5ee80d8d9d1b
Arnav Jha <arnavjha@Arnavs-MacBook-Pro-4.local>
2024-07-18 18:34:53 -0400

    mortgage changes pricing edit

--- components/pricingPage/index.tsx
+++ components/pricingPage/index.tsx
@@ -16,17 +16,16 @@ const PricingPage = () => {
         <NavBar/>
         <div className="bg-primary flex-1">
             <div className="bg-gray-50 py-20 px-6 sm:py-24 lg:px-8">
-            <div className="max-w-7xl mx-auto text-center">
+            <div className="max-w-4xl mx-auto text-center">
                 <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl lg:text-6xl">
                 Simple, Transparent Pricing
                 </h1>
                 <p className="mt-4 text-xl text-gray-500">
-                Choose the plan that fits your business needs. No hidden fees, no surprises.
+                One Plan. No hidden fees, No Surprises.
                 </p>
-                <div className="mt-16 mx-[15vw] grid gap-8 lg:grid-cols-2 lg:gap-x-8">
+                <div className="mt-16 mx-auto grid gap-8 lg:grid-cols-1 lg:gap-x-8">
                 <div className="relative bg-white py-8 px-6 border border-gray-200 rounded-lg shadow-sm">
                     <h2 className="text-lg leading-6 font-medium text-gray-900">Individual</h2>
-                    
                     <p className="mt-4 text-sm text-gray-500">Perfect for small teams or individual agents.</p>
                     <p className="mt-8 text-2xl font-extrabold text-gray-900/80 line-through">$299</p>
                     <p className="mt-2 text-base text-gray-900">Starting Offer:</p>
@@ -99,48 +98,6 @@ const PricingPage = () => {
                         </svg>
                         Access to Beta Features
                     </li>
-                    </ul>
-                </div>
-                <div className="relative bg-white py-8 px-6 border border-gray-200 rounded-lg shadow-sm">
-                    <h2 className="text-lg leading-6 font-medium text-gray-900">Enterprise</h2>
-                    <p className="mt-4 text-sm text-gray-500">Best for large teams or agencies.</p>
-                    <p className="mt-8 text-2xl font-extrabold text-gray-900/80 line-through">$1,499</p>
-                    <p className="mt-2 text-base text-gray-900">Starting Offer:</p>
-                    <p className="text-4xl font-extrabold text-gray-900">$499</p>
-                    <p className="mt-2 text-base text-gray-500">per month</p>
-                    <ul className="mt-8 space-y-4">
-                    <li className="flex items-center text-sm text-gray-700 font-bold">
-                    <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10 4a1 1 0 011 1v4h4a1 1 0 110 2h-4v4a1 1 0 11-2 0v-4H5a1 1 0 110-2h4V5a1 1 0 011-1z" clipRule="evenodd" />
-                    </svg>
-                        All Individual Plan Features
-                    </li>
-                    <li className="flex items-center text-sm text-gray-700">
-                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414L8 15.414 4.293 11.707a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                        </svg>
-                        Custom Team Management Features
-                    </li>
-                    <li className="flex items-center text-sm text-gray-700">
-                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414L8 15.414 4.293 11.707a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                        </svg>
-                        Personalized Phone Numbers
-                    </li>
-                    <li className="flex items-center text-sm text-gray-700">
-                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414L8 15.414 4.293 11.707a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                        </svg>
-                        Call Transcription
-                    </li>
-                    <li className="flex items-center text-sm text-gray-700">
-                        <svg className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414L8 15.414 4.293 11.707a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                        </svg>
-                        Dedicated IT Help Desk
-                    </li>
-
-
                     </ul>
                 </div>
                 </div>
